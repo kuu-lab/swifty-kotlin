a6a4fe9b238315ee7e18f85c3ab9b500d5829dcb
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-30 09:27:17 +0900

    Add CharSequence indexOfAny chars

--- Sources/RuntimeABI/RuntimeABISpec.swift
+++ Sources/RuntimeABI/RuntimeABISpec.swift
@@ -1030,6 +1030,17 @@ public enum RuntimeABISpec {
             section: "String"
         ),
         RuntimeABIFunctionSpec(
+            name: "kk_string_indexOfAny_chars",
+            parameters: [
+                RuntimeABIParameter(name: "strRaw", type: .intptr),
+                RuntimeABIParameter(name: "charsRaw", type: .intptr),
+                RuntimeABIParameter(name: "startIndex", type: .intptr),
+                RuntimeABIParameter(name: "ignoreCase", type: .intptr),
+            ],
+            returnType: .intptr,
+            section: "String"
+        ),
+        RuntimeABIFunctionSpec(
             name: "kk_string_indexOfFirst",
             parameters: [
                 RuntimeABIParameter(name: "strRaw", type: .intptr),
