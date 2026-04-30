7b32e22dbba6166e3381b5f3a431ce58754defbb
fumiya-kume <fumiya.kume@hotmail.com>
2026-04-30 09:47:11 +0900

    Add CharSequence lastIndexOfAny strings

--- Scripts/diff_cases/string_lastindexofany.kt
+++ Scripts/diff_cases/string_lastindexofany.kt
@@ -6,4 +6,10 @@ fun main() {
     println("abc".lastIndexOfAny(charArrayOf('x'), 2, false))
     println("abc".lastIndexOfAny(charArrayOf('C'), 2, true))
     println("abc".lastIndexOfAny(charArrayOf('a'), -1, false))
+    println(text.lastIndexOfAny(listOf("ot", "li"), 5, false))
+    println(text.lastIndexOfAny(listOf("KO"), 5, true))
+    println("abc".lastIndexOfAny(listOf("x", "bc"), 2, false))
+    println("abc".lastIndexOfAny(listOf(""), 5, false))
+    println("abc".lastIndexOfAny(listOf(""), 2, false))
+    println("abc".lastIndexOfAny(listOf("a"), -1, false))
 }
