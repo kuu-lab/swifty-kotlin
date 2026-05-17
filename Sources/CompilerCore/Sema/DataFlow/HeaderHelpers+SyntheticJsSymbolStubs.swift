4eee51e85381e9816dd4680af5b6e945eca08556
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2026-05-17 16:48:08 +0900

    Deduplicate JS annotation stub members (#2340)

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticJsSymbolStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticJsSymbolStubs.swift
@@ -26,9 +26,10 @@ extension DataFlowSemaPhase {
         }
 
         appendJsSymbolAnnotationMetadata(to: annotationSymbol, symbols: symbols)
-        registerJsSymbolNameMember(
+        registerSyntheticStringAnnotationPropertyAndConstructor(
             ownerSymbol: annotationSymbol,
             ownerFQName: kotlinJsPkg + [annotationName],
+            propertyName: "name",
             symbols: symbols,
             types: types,
             interner: interner
@@ -52,89 +53,5 @@ extension DataFlowSemaPhase {
             annotations.append(record)
         }
         symbols.setAnnotations(annotations, for: symbol)
-    }
-
-    private func registerJsSymbolNameMember(
-        ownerSymbol: SymbolID,
-        ownerFQName: [InternedString],
-        symbols: SymbolTable,
-        types: TypeSystem,
-        interner: StringInterner
-    ) {
-        let name = interner.intern("name")
-        let propertyFQName = ownerFQName + [name]
-        let propertySymbol: SymbolID
-        if let existing = symbols.lookup(fqName: propertyFQName) {
-            propertySymbol = existing
-        } else {
-            propertySymbol = symbols.define(
-                kind: .property,
-                name: name,
-                fqName: propertyFQName,
-                declSite: nil,
-                visibility: .public,
-                flags: [.synthetic]
-            )
-        }
-        symbols.setParentSymbol(ownerSymbol, for: propertySymbol)
-        symbols.setPropertyType(types.stringType, for: propertySymbol)
-
-        let initName = interner.intern("<init>")
-        let constructorFQName = ownerFQName + [initName]
-        let constructorSymbol: SymbolID
-        if let existing = symbols.lookupAll(fqName: constructorFQName).first(where: { symbolID in
-            guard let symbol = symbols.symbol(symbolID),
-                  symbol.kind == .constructor,
-                  let signature = symbols.functionSignature(for: symbolID)
-            else {
-                return false
-            }
-            return signature.parameterTypes == [types.stringType]
-        }) {
-            constructorSymbol = existing
-        } else {
-            constructorSymbol = symbols.define(
-                kind: .constructor,
-                name: initName,
-                fqName: constructorFQName,
-                declSite: nil,
-                visibility: .public,
-                flags: [.synthetic]
-            )
-        }
-        symbols.setParentSymbol(ownerSymbol, for: constructorSymbol)
-
-        let parameterFQName = constructorFQName + [name]
-        let parameterSymbol: SymbolID
-        if let existing = symbols.lookup(fqName: parameterFQName) {
-            parameterSymbol = existing
-        } else {
-            parameterSymbol = symbols.define(
-                kind: .valueParameter,
-                name: name,
-                fqName: parameterFQName,
-                declSite: nil,
-                visibility: .private,
-                flags: [.synthetic]
-            )
-        }
-        symbols.setParentSymbol(constructorSymbol, for: parameterSymbol)
-        symbols.setPropertyType(types.stringType, for: parameterSymbol)
-
-        let ownerType = types.make(.classType(ClassType(
-            classSymbol: ownerSymbol,
-            args: [],
-            nullability: .nonNull
-        )))
-        symbols.setFunctionSignature(
-            FunctionSignature(
-                parameterTypes: [types.stringType],
-                returnType: ownerType,
-                valueParameterSymbols: [parameterSymbol],
-                valueParameterHasDefaultValues: [false],
-                valueParameterIsVararg: [false]
-            ),
-            for: constructorSymbol
-        )
     }
 }
