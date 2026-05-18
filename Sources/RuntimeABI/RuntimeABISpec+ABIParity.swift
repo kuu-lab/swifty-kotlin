f827c493435727f04ac7b14ff79d295045d344f4
fumiya-kume <fumiya.kume@hotmail.com>
2026-05-18 14:20:38 +0900

    Add kk_enum_declaringJavaClass to RuntimeABISpec

--- Sources/RuntimeABI/RuntimeABISpec+ABIParity.swift
+++ Sources/RuntimeABI/RuntimeABISpec+ABIParity.swift
@@ -108,6 +108,9 @@ public extension RuntimeABISpec {
         abiParitySpec("kk_kclass_javaPrimitiveType", parameters: [
             p("kclassRaw", .intptr),
         ]),
+        abiParitySpec("kk_enum_declaringJavaClass", parameters: [
+            p("enumRaw", .intptr),
+        ]),
         abiParitySpec("kk_optional_toSet", parameters: [
             p("optionalRaw", .intptr),
         ]),
