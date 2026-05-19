6ab342cf0235a233feaf093765fa12e78a6dcd5d
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2026-05-19 16:44:36 +0900

    Add JVM KClass javaPrimitiveType stub (#2517)
    * Add JVM KClass javaPrimitiveType stub
    
    * Add kk_kclass_javaPrimitiveType to RuntimeABISpec
    
    * Add kk_kclass_javaPrimitiveType to allowlist

--- Tests/RuntimeTests/ABIMismatchTests+RuntimeExportParity.swift
+++ Tests/RuntimeTests/ABIMismatchTests+RuntimeExportParity.swift
@@ -70,6 +70,7 @@ extension ABIMismatchTests {
             "kk_kclass_has_annotation",
             "kk_kclass_java",
             "kk_kclass_javaClass",
+            "kk_kclass_javaPrimitiveType",
             "kk_kclass_js",
             "kk_kclass_register_annotation",
             "kk_long_range_firstOrNull",
