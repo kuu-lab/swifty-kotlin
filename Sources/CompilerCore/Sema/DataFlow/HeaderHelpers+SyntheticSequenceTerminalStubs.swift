e5e4e6dd5565609018c2e612e6a7dd5ebcdd7083
fumiya-kume <fumiya.kume@hotmail.com>
2026-05-22 10:26:32 +0900

    Fix comparatorType redeclaration in sequence terminal stubs

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticSequenceTerminalStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticSequenceTerminalStubs.swift
@@ -73,6 +73,20 @@ extension DataFlowSemaPhase {
             isSuspend: false,
             nullability: .nonNull
         )))
+        let comparatorTypeForSequence = if let comparatorSymbol = symbols.lookupByShortName(interner.intern("Comparator")).first {
+            types.make(.classType(ClassType(
+                classSymbol: comparatorSymbol,
+                args: [.invariant(typeParamType)],
+                nullability: .nonNull
+            )))
+        } else {
+            types.make(.functionType(FunctionType(
+                params: [typeParamType, typeParamType],
+                returnType: types.intType,
+                isSuspend: false,
+                nullability: .nonNull
+            )))
+        }
         let reduceIndexedOperationType = types.make(.functionType(FunctionType(
             params: [types.intType, typeParamType, typeParamType],
             returnType: typeParamType,
@@ -1976,7 +1990,7 @@ extension DataFlowSemaPhase {
 
         // maxWithOrNull(comparator): T?
         do {
-            let comparatorType = if let comparatorSymbol = symbols.lookupByShortName(interner.intern("Comparator")).first {
+            let comparatorTypeForSequence = if let comparatorSymbol = symbols.lookupByShortName(interner.intern("Comparator")).first {
                 types.make(.classType(ClassType(
                     classSymbol: comparatorSymbol,
                     args: [.invariant(typeParamType)],
@@ -1994,7 +2008,7 @@ extension DataFlowSemaPhase {
                 named: "maxWithOrNull",
                 externalLinkName: "kk_sequence_maxWithOrNull",
                 receiverType: receiverType,
-                parameters: [("comparator", comparatorType)],
+                parameters: [("comparator", comparatorTypeForSequence)],
                 returnType: types.makeNullable(typeParamType),
                 sequenceSymbol: sequenceSymbol,
                 sequenceFQName: sequenceFQName,
@@ -2005,6 +2019,22 @@ extension DataFlowSemaPhase {
                 flags: [.synthetic, .inlineFunction]
             )
         }
+
+        // minWith(comparator: Comparator<in T>): T
+        registerSequenceMemberStub(
+            named: "minWith",
+            externalLinkName: "kk_sequence_minWith",
+            receiverType: receiverType,
+            parameters: [("comparator", comparatorTypeForSequence)],
+            returnType: typeParamType,
+            sequenceSymbol: sequenceSymbol,
+            sequenceFQName: sequenceFQName,
+            typeParamSymbol: typeParamSymbol,
+            symbols: symbols,
+            interner: interner,
+            canThrow: true,
+            flags: [.synthetic, .inlineFunction]
+        )
 
         // unzip(): Pair<List<A>, List<B>> for Sequence<Pair<A, B>>
         let unzipName = interner.intern("unzip")
