a2ef6de75ed73759ceb64d72c0f21dceefe5712f
Kume Fumiya <fumiya.kume@hotmail.com>
2026-05-29 09:59:59 +0900

    Add ItemArrayLike asList surface

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticW3cDomItemArrayLikeStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticW3cDomItemArrayLikeStubs.swift
@@ -16,8 +16,16 @@ extension DataFlowSemaPhase {
             symbols: symbols,
             interner: interner
         )
-        _ = ensureItemArrayLikeInterface(
+        let itemArrayLike = ensureItemArrayLikeInterface(
             packageFQName: pkg,
+            symbols: symbols,
+            types: types,
+            interner: interner
+        )
+        registerItemArrayLikeAsList(
+            packageFQName: pkg,
+            packageSymbol: symbols.lookup(fqName: pkg),
+            itemArrayLikeSymbol: itemArrayLike.symbol,
             symbols: symbols,
             types: types,
             interner: interner
@@ -75,5 +83,97 @@ extension DataFlowSemaPhase {
         symbols.setPropertyType(interfaceType, for: interfaceSymbol)
 
         return (interfaceSymbol, typeParamSymbol)
+    }
+
+    private func registerItemArrayLikeAsList(
+        packageFQName: [InternedString],
+        packageSymbol: SymbolID?,
+        itemArrayLikeSymbol: SymbolID,
+        symbols: SymbolTable,
+        types: TypeSystem,
+        interner: StringInterner
+    ) {
+        let functionName = interner.intern("asList")
+        let functionFQName = packageFQName + [functionName]
+        let typeParameterName = interner.intern("T")
+        let typeParameterFQName = functionFQName + [typeParameterName]
+        let typeParameterSymbol: SymbolID
+        if let existing = symbols.lookup(fqName: typeParameterFQName),
+           symbols.symbol(existing)?.kind == .typeParameter {
+            typeParameterSymbol = existing
+        } else {
+            typeParameterSymbol = symbols.define(
+                kind: .typeParameter,
+                name: typeParameterName,
+                fqName: typeParameterFQName,
+                declSite: nil,
+                visibility: .private,
+                flags: [.synthetic]
+            )
+        }
+        symbols.setTypeParameterUpperBounds([types.anyType], for: typeParameterSymbol)
+        let elementType = types.make(.typeParam(TypeParamType(
+            symbol: typeParameterSymbol,
+            nullability: .nonNull
+        )))
+        let receiverType = types.make(.classType(ClassType(
+            classSymbol: itemArrayLikeSymbol,
+            args: [.out(elementType)],
+            nullability: .nonNull
+        )))
+
+        let collectionsPkg = ensurePackage(
+            path: ["kotlin", "collections"],
+            symbols: symbols,
+            interner: interner
+        )
+        guard let listSymbol = symbols.lookup(fqName: collectionsPkg + [interner.intern("List")]) else {
+            return
+        }
+        let returnType = types.make(.classType(ClassType(
+            classSymbol: listSymbol,
+            args: [.out(elementType)],
+            nullability: .nonNull
+        )))
+
+        if symbols.lookupAll(fqName: functionFQName).contains(where: { symbolID in
+            guard symbols.symbol(symbolID)?.kind == .function,
+                  let signature = symbols.functionSignature(for: symbolID)
+            else {
+                return false
+            }
+            return signature.receiverType == receiverType
+                && signature.parameterTypes.isEmpty
+                && signature.returnType == returnType
+        }) {
+            return
+        }
+
+        let functionSymbol = symbols.define(
+            kind: .function,
+            name: functionName,
+            fqName: functionFQName,
+            declSite: nil,
+            visibility: .public,
+            flags: [.synthetic]
+        )
+        if let packageSymbol {
+            symbols.setParentSymbol(packageSymbol, for: functionSymbol)
+        }
+        symbols.setParentSymbol(functionSymbol, for: typeParameterSymbol)
+        symbols.setFunctionSignature(
+            FunctionSignature(
+                receiverType: receiverType,
+                parameterTypes: [],
+                returnType: returnType,
+                typeParameterSymbols: [typeParameterSymbol],
+                typeParameterUpperBoundsList: [[types.anyType]],
+                valueParameterSymbols: [],
+                valueParameterHasDefaultValues: [],
+                valueParameterIsVararg: []
+            ),
+            for: functionSymbol
+        )
+        symbols.setExternalLinkName("kk_dom_itemArrayLike_asList", for: functionSymbol)
     }
 }
