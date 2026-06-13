1aa00f767a6e7363c9bde30f72a1602417bd61d3
fumiya-kume <fumiya.kume@hotmail.com>
2026-06-13 12:25:43 +0900

    Remove JsCollectionsReadonlySetStubs + move helpers to ToMutableSetStubs, delete tests

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticJsCollectionsReadonlySetToMutableSetStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticJsCollectionsReadonlySetToMutableSetStubs.swift
@@ -103,4 +103,72 @@ extension DataFlowSemaPhase {
             for: functionSymbol
         )
     }
+
+    func ensureJsReadonlySetForConversions(
+        packageFQName: [InternedString],
+        symbols: SymbolTable,
+        types: TypeSystem,
+        interner: StringInterner
+    ) -> (symbol: SymbolID, typeParameterSymbol: SymbolID) {
+        let interfaceName = interner.intern("JsReadonlySet")
+        let interfaceFQName = packageFQName + [interfaceName]
+        let interfaceSymbol = ensureInterfaceSymbol(
+            named: "JsReadonlySet",
+            in: packageFQName,
+            symbols: symbols,
+            interner: interner
+        )
+        if let packageSymbol = symbols.lookup(fqName: packageFQName) {
+            symbols.setParentSymbol(packageSymbol, for: interfaceSymbol)
+        }
+        appendJsCollectionsReadonlySetAnnotation(to: interfaceSymbol, symbols: symbols)
+
+        let typeParamName = interner.intern("E")
+        let typeParamFQName = interfaceFQName + [typeParamName]
+        let typeParamSymbol: SymbolID
+        if let existing = symbols.lookup(fqName: typeParamFQName),
+           symbols.symbol(existing)?.kind == .typeParameter {
+            typeParamSymbol = existing
+        } else {
+            typeParamSymbol = symbols.define(
+                kind: .typeParameter,
+                name: typeParamName,
+                fqName: typeParamFQName,
+                declSite: nil,
+                visibility: .private,
+                flags: [.synthetic]
+            )
+        }
+        symbols.setParentSymbol(interfaceSymbol, for: typeParamSymbol)
+
+        let typeParamType = types.make(.typeParam(TypeParamType(
+            symbol: typeParamSymbol,
+            nullability: .nonNull
+        )))
+        let interfaceType = types.make(.classType(ClassType(
+            classSymbol: interfaceSymbol,
+            args: [.out(typeParamType)],
+            nullability: .nonNull
+        )))
+
+        types.setNominalTypeParameterSymbols([typeParamSymbol], for: interfaceSymbol)
+        types.setNominalTypeParameterVariances([.out], for: interfaceSymbol)
+        symbols.setPropertyType(interfaceType, for: interfaceSymbol)
+
+        return (interfaceSymbol, typeParamSymbol)
+    }
+
+    func appendJsCollectionsReadonlySetAnnotation(
+        to symbol: SymbolID,
+        symbols: SymbolTable
+    ) {
+        let experimentalRecord = MetadataAnnotationRecord(
+            annotationFQName: "kotlin.js.ExperimentalJsCollectionsApi"
+        )
+        var annotations = symbols.annotations(for: symbol)
+        if !annotations.contains(experimentalRecord) {
+            annotations.append(experimentalRecord)
+            symbols.setAnnotations(annotations, for: symbol)
+        }
+    }
 }
