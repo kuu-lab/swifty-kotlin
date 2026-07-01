d3fbb363670753eeb02ef3df2074b5a655583448
fumiya-kume <fumiya.kume@hotmail.com>
2026-07-02 03:41:33 +0900

    Stabilize Sema golden normalization

--- Tests/CompilerCoreTests/GoldenHarnessSemaComparisonNormalizationTests.swift
+++ Tests/CompilerCoreTests/GoldenHarnessSemaComparisonNormalizationTests.swift
@@ -69,6 +69,46 @@ struct GoldenHarnessSemaComparisonNormalizationTests {
     }
 
     @Test
+    func normalizesExpressionDisambiguatorSuffixes() {
+        let expected = """
+        expr e@17:15#1 call callee=e@17:15#0 args=[_:e@17:37,_:e@17:40,_:e@17:46#1] type=sample.Value
+        expr e@19:22 string(no message) type=String
+        expr e@17:9#6 throw value=e@17:15#1 type=Nothing
+        """
+
+        let actual = """
+        expr e@17:15#3 call callee=e@17:15#2 args=[_:e@17:37,_:e@17:40,_:e@17:46#3] type=sample.Value
+        expr e@19:22#1 string(no message) type=String
+        expr e@17:9#8 throw value=e@17:15#3 type=Nothing
+        """
+
+        #expect(
+            GoldenHarness.normalizedForComparison(suiteName: "Sema", output: actual)
+                == GoldenHarness.normalizedForComparison(suiteName: "Sema", output: expected)
+        )
+    }
+
+    @Test
+    func normalizesObjectLiteralOrdinals() {
+        let expected = """
+        symbol fq=__ObjectLiteral_13_74_110 kind=class vis=private flags=synthetic
+        symbol fq=__ObjectLiteral_13_74_110.value kind=property vis=public flags=synthetic type=Int
+        expr e@4:17#2 objectLiteral supers=[] type=__ObjectLiteral_13_74_110
+        """
+
+        let actual = """
+        symbol fq=__ObjectLiteral_14_74_110 kind=class vis=private flags=synthetic
+        symbol fq=__ObjectLiteral_14_74_110.value kind=property vis=public flags=synthetic type=Int
+        expr e@4:17#9 objectLiteral supers=[] type=__ObjectLiteral_14_74_110
+        """
+
+        #expect(
+            GoldenHarness.normalizedForComparison(suiteName: "Sema", output: actual)
+                == GoldenHarness.normalizedForComparison(suiteName: "Sema", output: expected)
+        )
+    }
+
+    @Test
     func normalizationIsIdempotent() {
         let input = """
         symbol fq=sample.Box kind=class vis=public flags=_
