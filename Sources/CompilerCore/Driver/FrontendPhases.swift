dc7964bdfa81223ac470fd5f7873fff05c6e5d4c
Kume Fumiya <fumiya.kume@hotmail.com>
2026-07-08 03:42:33 +0900

    Inject range membership stdlib source

--- Sources/CompilerCore/Driver/FrontendPhases.swift
+++ Sources/CompilerCore/Driver/FrontendPhases.swift
@@ -114,7 +114,6 @@ final class LoadSourcesPhase: CompilerPhase {
         "kotlin/comparisons/Comparators",
         "kotlin/logging/AdvancedLogger",
         "kotlin/ranges/RangeIterators",
-        "kotlin/ranges/RangeMembership",
         "kotlin/reflect/KClassAnnotationRegistration",
         "kotlin/text/StringBuilder",
         "kotlin/text/StringIndentFormat",
