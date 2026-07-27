53c837d99b14d5ef506001218eebf430bffd834c
fumiya-kume <fumiya.kume@hotmail.com>
2026-07-27 15:14:15 +0900

    Remove calls to deleted synthetic native annotation/data stub functions

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticNativeInteropStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticNativeInteropStubs.swift
@@ -6,35 +6,7 @@ extension DataFlowSemaPhase {
         types: TypeSystem,
         interner: StringInterner
     ) {
-        registerSyntheticNativeExperimentalAnnotations(
-            symbols: symbols,
-            interner: interner
-        )
-        registerSyntheticNativeObjCAnnotations(
-            symbols: symbols,
-            interner: interner
-        )
         registerSyntheticCInteropStubs(
-            symbols: symbols,
-            types: types,
-            interner: interner
-        )
-        registerSyntheticNativeIdentityHashCodeStub(
-            symbols: symbols,
-            types: types,
-            interner: interner
-        )
-        registerSyntheticNativeStackTraceAddressStub(
-            symbols: symbols,
-            types: types,
-            interner: interner
-        )
-        registerSyntheticNativeUnhandledExceptionHookStubs(
-            symbols: symbols,
-            types: types,
-            interner: interner
-        )
-        registerSyntheticNativeByteArrayAccessorStubs(
             symbols: symbols,
             types: types,
             interner: interner
