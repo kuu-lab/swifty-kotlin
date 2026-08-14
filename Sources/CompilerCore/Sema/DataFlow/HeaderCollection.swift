10b2a4802e4353aad93b37d69c3f424919d87b36
Kume Fumiya <fumiya.kume@hotmail.com>
2026-08-14 20:37:38 +0900

    KSP-650: Kotlinize test time sources

--- Sources/CompilerCore/Sema/DataFlow/HeaderCollection.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderCollection.swift
@@ -1170,6 +1170,12 @@ extension DataFlowSemaPhase {
                 ["kotlin", "time", "TimeSource", "WithComparableMarks"],
                 ["kotlin", "time", "TimeSource", "Monotonic"],
             ]
+        case "__bundled_kotlin/time/TimeSources.kt":
+            [
+                ["kotlin", "time", "AbstractLongTimeSource"],
+                ["kotlin", "time", "AbstractDoubleTimeSource"],
+                ["kotlin", "time", "TestTimeSource"],
+            ]
         case "__bundled_kotlin/sequences/Sequence.kt":
             [["kotlin", "sequences", "Sequence"]]
         case "__bundled_kotlin/Tuples.kt":
