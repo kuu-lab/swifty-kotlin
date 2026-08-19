0ef11b717ad7d8db4d29628e29b69ab40b5b995b
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2026-08-19 08:41:12 +0000

    Migrate ubyteArrayOf to the Kotlin stdlib (#5949)

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticArrayStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticArrayStubs.swift
@@ -1149,7 +1149,6 @@ extension DataFlowSemaPhase {
             ("booleanArrayOf", "BooleanArray", types.booleanType),
             ("charArrayOf", "CharArray", types.charType),
             ("shortArrayOf", "ShortArray", types.shortType),
-            ("ubyteArrayOf", "UByteArray", types.ubyteType),
             ("ushortArrayOf", "UShortArray", types.ushortType),
             ("uintArrayOf", "UIntArray", types.uintType),
             ("ulongArrayOf", "ULongArray", types.ulongType),
