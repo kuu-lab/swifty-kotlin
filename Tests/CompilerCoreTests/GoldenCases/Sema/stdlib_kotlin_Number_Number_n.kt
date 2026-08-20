1be3848e1f8af4cc65448c6d9b4b59cfff046ef4
Kume Fumiya <fumiya.kume@hotmail.com>
2026-08-20 10:12:02 +0900

    Add Kotlin Number conversion API coverage

---
+++ Tests/CompilerCoreTests/GoldenCases/Sema/stdlib_kotlin_Number_Number_n.kt
@@ -0,0 +1,11 @@
+package golden.sema
+
+@Suppress("DEPRECATION")
+fun numberToChar(n: Number): Char = n.toChar()
+
+fun numberToDouble(n: Number): Double = n.toDouble()
+fun numberToFloat(n: Number): Float = n.toFloat()
+fun numberToInt(n: Number): Int = n.toInt()
+fun numberToLong(n: Number): Long = n.toLong()
+fun numberToShort(n: Number): Short = n.toShort()
+fun numberToByte(n: Number): Byte = n.toByte()
