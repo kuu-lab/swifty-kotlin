8527dca3f637e8361fa5f60a3390270a61a7ae14
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2026-08-21 20:40:44 +0000

    Expose the source-backed Result constructor (#5977)
    * Expose source-backed Result constructor
    
    * Expand Lazy regression coverage
    
    * Fix case-sensitive Lazy Golden file ID
    
    * Fix Result constructor runtime lowering
    
    * Restore Lazy regression coverage
    
    * Update Result constructor migration expectations

--- Sources/CompilerCore/Sema/DataFlow/HeaderCollection.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderCollection.swift
@@ -1279,7 +1279,7 @@ extension DataFlowSemaPhase {
             [["kotlin", "collections", "AbstractCollection"]]
         case "__bundled_kotlin/collections/AbstractMutableCollection.kt":
             [["kotlin", "collections", "AbstractMutableCollection"]]
-        case "__bundled_kotlin/Result.kt":
+        case "__bundled_kotlin/Result/Stdlib.kt":
             [["kotlin", "Result"]]
         case "__bundled_kotlin/text/StringBuilder.kt":
             [["kotlin", "text", "StringBuilder"]]
