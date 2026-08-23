a51e989629d28e58a3f63deffe87861a432c78ed
Kume Fumiya <fumiya.kume@hotmail.com>
2026-08-23 12:22:17 +0900

    KSP-887: implement Short companion constants

---
+++ Scripts/diff_cases/stdlib_kotlin_Short_Companion_Companion_n.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println(Short.MAX_VALUE)
+    println(Short.MIN_VALUE)
+    println(Short.SIZE_BITS)
+    println(Short.SIZE_BYTES)
+    println(Short.Companion.MAX_VALUE)
+    println(Short.Companion.MIN_VALUE)
+    println(Short.Companion.SIZE_BITS)
+    println(Short.Companion.SIZE_BYTES)
+    val boxedMax: Any = Short.MAX_VALUE
+    val boxedMin: Any = Short.MIN_VALUE
+    println(boxedMax)
+    println(boxedMin)
+}
