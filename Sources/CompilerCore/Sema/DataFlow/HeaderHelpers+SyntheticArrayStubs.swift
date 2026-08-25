eddfffc80fee959263ab71627063835b0be2514a
Kume Fumiya <fumiya.kume@hotmail.com>
2026-08-25 15:09:10 +0900

    KSP-769: implement source-backed booleanArrayOf

--- Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticArrayStubs.swift
+++ Sources/CompilerCore/Sema/DataFlow/HeaderHelpers+SyntheticArrayStubs.swift
@@ -875,7 +875,6 @@ extension DataFlowSemaPhase {
         }
 
         let primitiveArrayFactoryTypes: [(String, String, TypeID)] = [
-            ("booleanArrayOf", "BooleanArray", types.booleanType),
             ("uintArrayOf", "UIntArray", types.uintType),
             ("ulongArrayOf", "ULongArray", types.ulongType),
         ]
