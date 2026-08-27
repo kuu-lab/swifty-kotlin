932356ea86bad519f9a723bc25439a32729d3400
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2026-08-27 23:03:20 +0000

    KSP-1440: implement StringBuilder receiver APIs (#6297)
    * KSP-1440: implement StringBuilder receiver APIs
    
    * Fix StringBuilder self-insert overlap semantics
    
    * Update StringBuilder overload link regression
    
    * Update StringBuilder Sema goldens

--- Sources/CompilerCore/Stdlib/kotlin/text/StringBuilder.kt
+++ Sources/CompilerCore/Stdlib/kotlin/text/StringBuilder.kt
@@ -50,6 +50,18 @@ public class StringBuilder {
     fun append(value: Double): StringBuilder =
         __kk_string_builder_append_obj(value.toString())
 
+    fun append(value: Any?): StringBuilder =
+        __kk_string_builder_append_obj(value)
+
+    fun append(value: Byte): StringBuilder =
+        __kk_string_builder_append_obj(value.toString())
+
+    fun append(value: Short): StringBuilder =
+        __kk_string_builder_append_obj(value.toString())
+
+    fun append(value: CharArray): StringBuilder =
+        appendRange(value, 0, value.size)
+
     fun append(vararg value: Any?): StringBuilder {
         var index = 0
         while (index < value.size) {
@@ -94,6 +106,18 @@ public class StringBuilder {
     fun insert(index: Int, value: Double): StringBuilder =
         insertString(index, value.toString())
 
+    fun insert(index: Int, value: Byte): StringBuilder =
+        insertString(index, value.toString())
+
+    fun insert(index: Int, value: Short): StringBuilder =
+        insertString(index, value.toString())
+
+    fun insert(index: Int, value: CharArray): StringBuilder =
+        insertRange(index, value, 0, value.size)
+
+    fun insert(index: Int, value: CharSequence?): StringBuilder =
+        __kk_string_builder_insert_char_sequence(index, value)
+
     fun delete(startIndex: Int, endIndex: Int): StringBuilder {
         checkRange(startIndex, endIndex)
         val current = toString()
@@ -135,8 +159,45 @@ public class StringBuilder {
         return this
     }
 
+    fun appendRange(value: CharArray, startIndex: Int, endIndex: Int): StringBuilder =
+        __kk_string_builder_append_char_array(value, startIndex, endIndex)
+
     fun insertRange(index: Int, value: CharSequence, startIndex: Int, endIndex: Int): StringBuilder =
         insertString(index, (value as String).substring(startIndex, endIndex))
+
+    fun insertRange(index: Int, value: CharArray, startIndex: Int, endIndex: Int): StringBuilder =
+        __kk_string_builder_insert_char_array(index, value, startIndex, endIndex)
+
+    fun indexOf(string: String): Int =
+        __kk_string_builder_index_of(string, 0)
+
+    fun indexOf(string: String, startIndex: Int): Int =
+        __kk_string_builder_index_of(string, startIndex)
+
+    fun lastIndexOf(string: String): Int =
+        __kk_string_builder_last_index_of(string, __kk_string_builder_length_utf16())
+
+    fun lastIndexOf(string: String, startIndex: Int): Int =
+        __kk_string_builder_last_index_of(string, startIndex)
+
+    fun setLength(newLength: Int): Unit {
+        __kk_string_builder_set_length(newLength)
+    }
+
+    fun substring(startIndex: Int): String =
+        __kk_string_builder_substring(startIndex, __kk_string_builder_length_utf16())
+
+    fun substring(startIndex: Int, endIndex: Int): String =
+        __kk_string_builder_substring(startIndex, endIndex)
+
+    fun toCharArray(
+        destination: CharArray,
+        destinationOffset: Int = 0,
+        startIndex: Int = 0,
+        endIndex: Int = __kk_string_builder_length_utf16()
+    ): Unit {
+        __kk_string_builder_to_char_array(destination, destinationOffset, startIndex, endIndex)
+    }
 
     fun setRange(startIndex: Int, endIndex: Int, value: String): StringBuilder =
         replaceString(startIndex, endIndex, value, false)
@@ -162,9 +223,7 @@ public class StringBuilder {
         __kk_string_builder_toString()
 
     private fun insertString(index: Int, value: String): StringBuilder {
-        checkInsertIndex(index)
-        val current = toString()
-        return resetTo(current.substring(0, index) + value + current.substring(index))
+        return __kk_string_builder_insert_obj(index, value)
     }
 
     private fun replaceString(startIndex: Int, endIndex: Int, value: String, clampEnd: Boolean): StringBuilder {
@@ -210,6 +269,36 @@ public class StringBuilder {
 
     @KsSymbolName("__kk_string_builder_append_obj")
     private external fun __kk_string_builder_append_obj(value: Any?): StringBuilder
+
+    @KsSymbolName("__kk_string_builder_append_char_array")
+    private external fun __kk_string_builder_append_char_array(value: CharArray, startIndex: Int, endIndex: Int): StringBuilder
+
+    @KsSymbolName("__kk_string_builder_insert_obj")
+    private external fun __kk_string_builder_insert_obj(index: Int, value: Any?): StringBuilder
+
+    @KsSymbolName("__kk_string_builder_insert_char_sequence")
+    private external fun __kk_string_builder_insert_char_sequence(index: Int, value: CharSequence?): StringBuilder
+
+    @KsSymbolName("__kk_string_builder_insert_char_array")
+    private external fun __kk_string_builder_insert_char_array(index: Int, value: CharArray, startIndex: Int, endIndex: Int): StringBuilder
+
+    @KsSymbolName("__kk_string_builder_index_of")
+    private external fun __kk_string_builder_index_of(value: String, startIndex: Int): Int
+
+    @KsSymbolName("__kk_string_builder_last_index_of")
+    private external fun __kk_string_builder_last_index_of(value: String, startIndex: Int): Int
+
+    @KsSymbolName("__kk_string_builder_set_length")
+    private external fun __kk_string_builder_set_length(newLength: Int): Unit
+
+    @KsSymbolName("__kk_string_builder_substring")
+    private external fun __kk_string_builder_substring(startIndex: Int, endIndex: Int): String
+
+    @KsSymbolName("__kk_string_builder_to_char_array")
+    private external fun __kk_string_builder_to_char_array(destination: CharArray, destinationOffset: Int, startIndex: Int, endIndex: Int): Unit
+
+    @KsSymbolName("__kk_string_builder_length_utf16")
+    private external fun __kk_string_builder_length_utf16(): Int
 
     @KsSymbolName("__kk_string_builder_toString")
     private external fun __kk_string_builder_toString(): String
