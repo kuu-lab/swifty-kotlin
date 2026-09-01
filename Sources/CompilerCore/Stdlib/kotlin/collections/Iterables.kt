c5cdfad492efa680b7da4406b68d7357e0576982
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2026-09-01 18:02:53 +0000

    Implement Iterable first-family stdlib APIs (KSP-973) (#6177)
    * Implement Iterable first-family stdlib APIs (KSP-973)
    
    * Update Collection firstOrNull regression expectation
    
    * Update Golden expectations for Iterable first APIs

--- Sources/CompilerCore/Stdlib/kotlin/collections/Iterables.kt
+++ Sources/CompilerCore/Stdlib/kotlin/collections/Iterables.kt
@@ -247,6 +247,30 @@ public inline fun <T, R, C : MutableCollection<in R>> Iterable<T>.flatMapTo(
 
 public fun <T> Collection<T>.isNotEmpty(): Boolean = !isEmpty()
 
+public fun <T> Iterable<T>.first(): T {
+    for (element in this) return element
+    throw NoSuchElementException("Collection is empty.")
+}
+
+public inline fun <T> Iterable<T>.first(predicate: (T) -> Boolean): T {
+    for (element in this) {
+        if (predicate(element)) return element
+    }
+    throw NoSuchElementException("Collection contains no element matching the predicate.")
+}
+
+public fun <T> Iterable<T>.firstOrNull(): T? {
+    for (element in this) return element
+    return null
+}
+
+public inline fun <T> Iterable<T>.firstOrNull(predicate: (T) -> Boolean): T? {
+    for (element in this) {
+        if (predicate(element)) return element
+    }
+    return null
+}
+
 @Suppress("UNCHECKED_CAST")
 public fun <T> Iterable<T>.last(): T {
     var found = false
@@ -485,7 +509,8 @@ public inline fun <T> Iterable<T>.count(predicate: (T) -> Boolean): Int {
     return count
 }
 
-public fun <T, R : Any> Iterable<T>.firstNotNullOfOrNull(transform: (T) -> R?): R? {
+@SinceKotlin("1.5")
+public inline fun <T, R : Any> Iterable<T>.firstNotNullOfOrNull(transform: (T) -> R?): R? {
     for (element in this) {
         val result = transform(element)
         if (result != null) return result
@@ -493,7 +518,8 @@ public fun <T, R : Any> Iterable<T>.firstNotNullOfOrNull(transform: (T) -> R?): 
     return null
 }
 
-public fun <T, R : Any> Iterable<T>.firstNotNullOf(transform: (T) -> R?): R {
+@SinceKotlin("1.5")
+public inline fun <T, R : Any> Iterable<T>.firstNotNullOf(transform: (T) -> R?): R {
     for (element in this) {
         val result = transform(element)
         if (result != null) return result
