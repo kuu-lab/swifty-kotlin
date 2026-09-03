68a5d75e42daa37ace9cad165eee6e1424e4ea47
Kume Fumiya <fumiya.kume@hotmail.com>
2026-09-04 05:30:53 +0900

    KSP-1497: source-back TimedValue data class

---
+++ Scripts/diff_cases/stdlib_kotlin_time_TimedValue_TimedValue_n.kt
@@ -0,0 +1,27 @@
+// Diff regression for kotlin.time.TimedValue receiver members.
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.TimedValue
+
+fun main() {
+    val tv = TimedValue("hello", 5.seconds)
+    val (v, d) = tv
+    val copied = tv.copy()
+    val valueChanged = tv.copy(value = "world")
+    val durationChanged = tv.copy(duration = 10.seconds)
+    val bothChanged = tv.copy("changed", 1.seconds)
+
+    println(tv.value)
+    println(tv.duration)
+    println(tv.component1())
+    println(tv.component2())
+    println(tv.toString())
+    println(v)
+    println(d)
+    println(tv.equals(copied))
+    println(copied == tv)
+    println(copied.hashCode() == tv.hashCode())
+    println(valueChanged)
+    println(valueChanged == tv)
+    println(durationChanged)
+    println(bothChanged)
+}
