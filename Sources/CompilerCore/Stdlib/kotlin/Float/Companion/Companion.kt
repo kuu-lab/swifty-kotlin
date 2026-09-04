598c9819b6d687e330580493ea22a724ce2d2d28
fumiya-kume <fumiya.kume@hotmail.com>
2026-09-05 05:10:00 +0900

    Fix Float.Companion.NaN bit pattern and update Sema goldens

--- Sources/CompilerCore/Stdlib/kotlin/Float/Companion/Companion.kt
+++ Sources/CompilerCore/Stdlib/kotlin/Float/Companion/Companion.kt
@@ -11,7 +11,7 @@ public val Float.Companion.MIN_VALUE: Float get() = 1.4E-45F
 // Float arithmetic across the raw ABI boundary.
 public val Float.Companion.NEGATIVE_INFINITY: Float get() = fromBits(-8388608)
 
-public val Float.Companion.NaN: Float get() = -(0.0F / 0.0F)
+public val Float.Companion.NaN: Float get() = fromBits(2143289344)
 
 public val Float.Companion.POSITIVE_INFINITY: Float get() = 1.0F / 0.0F
 
